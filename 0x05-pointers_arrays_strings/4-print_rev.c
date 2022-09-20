9b90c860fee03318b7924c49d2ccb956e4e82562
Ode-Asumeng <odemaame18@gmail.com>
2022-09-20 11:55:53 -0700

    corr

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,26 +1,17 @@
 #include "main.h"
 /**
- * rev_string - function
+ * print_rev - function
  *
  * @s: pointer of parameter
  */
-void rev_string(char *s)
+void print_rev(char *s)
 {
-	char temp;
 	int length = 0;
-	int b = 0;
-	int e;
 
 	while (s[length] != '\0')
 		length++;
-	e = length - 1;
-	length /= 2;
-	while (length--)
-	{
-		temp = s[b];
-		s[b] = s[e];
-		s[e] = temp;
-		b++;
-		e--;
-	}
+	while (length > 0)
+		_putchar(s[--length]);
+	_putchar(10);
 }
+
